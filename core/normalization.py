e0d6a8e8d38dbd70e3eabd89e49fb04b14ab5861
Khroneq <61234580+Khroneq@users.noreply.github.com>
2025-09-16 20:07:38 +0530

    normalization to keep things pretty

---
+++ core/normalization.py
@@ -0,0 +1,11 @@
+def normalised_findings(service: str, check_id: str, title: str, resource: str, status: str, notes: str = "") -> dict:
+    return {
+        "service": service,
+        "check_id": check_id,
+        "title": title,
+        "resource": resource,
+        "status": status, #PASS / FAIL / MANUAL
+        "notes": notes
+    }
+
+#expand this for other services once a basic AWS scanner is ready, or create separate files for each service(???)
